212aed085a86c7cd3c7f5f2f5ca6cd557abea839
John Marino <github@marino.st>
2011-10-16 13:36:56 +0200

    Continued work on package
    Lots of issues were discovered as the package got tested, mainly
    related to my still relative unfamiliarity with this complex
    pkgsrc system.

--- pkgsrc/pkgtools/tinderbox-dragonfly/options.mk
+++ pkgsrc/pkgtools/tinderbox-dragonfly/options.mk
@@ -27,24 +27,25 @@ PKG_FAIL_REASON+=	"Please select one of need-pgsql, have-pgsql, need-mysql, have
 .include "../../mk/mysql.buildlink3.mk"
 .endif
 
-.if !empty(PKG_OPTIONS:Mneed-pgsql) || !empty(PKG_OPTIONS:Mhave-pgsql)
-DEPENDS+=	DBD-Pg>=2.12:../../databases/p5-DBD-postgresql
 .if !empty(PKG_OPTIONS:Mwebui)
-DEPENDS+=	MDB2_Driver_pgsql>1.5:../../databases/pear-MDB2_Driver_pgsql
-DEPENDS+=	php-pgsql>=5.1:../../databases/php-pgsql
+PLIST.WEBUI=	yes
+.include "../../lang/php/phpversion.mk"
+.endif
+
+.if !empty(PKG_OPTIONS:Mneed-pgsql) || !empty(PKG_OPTIONS:Mhave-pgsql)
+DEPENDS+=	p5-DBD-postgresql>=2.12:../../databases/p5-DBD-postgresql
+.if !empty(PKG_OPTIONS:Mwebui)
+DEPENDS+=	${PHP_PKG_PREFIX}-pear-MDB2_Driver_pgsql:../../databases/pear-MDB2_Driver_pgsql
+DEPENDS+=	${PHP_PKG_PREFIX}-pgsql>=5.1:../../databases/php-pgsql
 .endif
 .endif
 
 .if !empty(PKG_OPTIONS:Mneed-mysql) || !empty(PKG_OPTIONS:Mhave-mysql)
-DEPENDS+=	DBD-mysql>=4:../../databases/p5-DBD-mysql
+DEPENDS+=	p5-DBD-mysql>=4:../../databases/p5-DBD-mysql
 .if !empty(PKG_OPTIONS:Mwebui)
-DEPENDS+=	MDB2_Driver_mysql>1.5:../../databases/pear-MDB2_Driver_mysql
-DEPENDS+=	php-mysql>=5.1:../../databases/php-mysql
+DEPENDS+=	${PHP_PKG_PREFIX}-perar-MDB2_Driver_mysql>1.5:../../databases/pear-MDB2_Driver_mysql
+DEPENDS+=	${PHP_PKG_PREFIX}-mysql>=5.1:../../databases/php-mysql
 .endif
-.endif
-
-.if !empty(PKG_OPTIONS:Mwebui)
-PLIST.WEBUI=	yes
 .endif
 
 #####################
