a9afb6276ea88d054138d3a9c434c5b56f84ccc3
John Marino <github@marino.st>
2011-10-16 18:26:56 +0200

    Fixing pgsql/release candidate issues
    It sames that RC1 is a bit rough with badly escaped SQL instructions.
    This broke ./tc Setup

--- instruments/dfly_patches/gen_patches.sh
+++ instruments/dfly_patches/gen_patches.sh
@@ -15,10 +15,12 @@ cd ${DFLYDIR}
 while read mapline; do
 	patch_id=`echo $mapline | /usr/bin/sed "s/:.*//"`
 	target=`echo $mapline | /usr/bin/sed "s/[^\w]*://"`
-	/usr/pkg/bin/pkgdiff ../FreeBSD/${target} ${target} > ${PATCHDIR}/patch-${patch_id} 
+	if [ -f ${target} ]; then
+	   /usr/pkg/bin/pkgdiff ../FreeBSD/${target} ${target} > ${PATCHDIR}/patch-${patch_id}
+	fi
 done < ${SCRIPTDIR}/patch.map
 
-rm ${REALPKGLOC}/patches/*
+rm -f ${REALPKGLOC}/patches/*
 cp ${PATCHDIR}/* ${REALPKGLOC}/patches
 cd ${REALPKGLOC}
 /usr/pkg/bin/bmake distinfo
