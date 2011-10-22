3da8b01242cfc27aa5ce054ef6b409826f3cf5e7
John Marino <github@marino.st>
2011-10-22 22:38:22 +0200

    Upgrade FreeBSD to release candidate 3.4r3

--- FreeBSD/lib/Tinderbox/TinderboxDS.pm
+++ FreeBSD/lib/Tinderbox/TinderboxDS.pm
@@ -23,7 +23,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/lib/Tinderbox/TinderboxDS.pm,v 1.88.2.6 2011/01/16 02:06:22 marcus Exp $
+# $MCom: portstools/tinderbox/lib/Tinderbox/TinderboxDS.pm,v 1.88.2.7 2011/10/22 08:02:08 marcus Exp $
 #
 
 package Tinderbox::TinderboxDS;
@@ -1646,6 +1646,13 @@ sub removeBuild {
         }
 
         $rc = $self->_doQuery("DELETE FROM build_users WHERE build_id=?",
+                [$build->getId()]);
+
+        if (!$rc) {
+                return $rc;
+        }
+
+        $rc = $self->_doQuery("DELETE FROM build_ports_queue WHERE build_id=?",
                 [$build->getId()]);
 
         if (!$rc) {
--- FreeBSD/lib/tc_command.sh
+++ FreeBSD/lib/tc_command.sh
@@ -24,7 +24,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/lib/tc_command.sh,v 1.101.2.44 2011/10/16 00:52:01 marcus Exp $
+# $MCom: portstools/tinderbox/lib/tc_command.sh,v 1.101.2.45 2011/10/17 01:01:23 marcus Exp $
 #
 
 export _defaultUpdateHost="cvsup18.FreeBSD.org"
@@ -327,9 +327,9 @@ Setup () {
 #---------------------------------------------------------------------------
 
 Upgrade () {
-    VERSION="3.2.5"
+    VERSION="3.2.6"
     TINDERBOX_URL="http://tinderbox.marcuscom.com/"
-    DB_MIGRATION_PATH="3.0 3.1 3.2 3.2.1 3.2.2 3.2.3 3.2.4 ${VERSION}"
+    DB_MIGRATION_PATH="3.0 3.1 3.2 3.2.1 3.2.2 3.2.3 3.2.4 3.2.5 ${VERSION}"
 
     bkup_file=""
 
